33a517f07e7c1af26e23340c609f90b58ca19fea
Ambrozik <ambrozik@FRLFK0635003719.corp.capgemini.com>
2025-01-08 14:09:24 +0100

    up

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import { UserComponent } from './user.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-import { LoadingComponent } from '../shared/loading/loading.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent,
     LoginComponent
   ],
   imports: [
